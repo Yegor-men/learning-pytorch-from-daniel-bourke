2b343574867ba93646099dc749493e0d52f2f1f3
Yegor Men <yegormenovchshikov@gmail.com>
2025-01-06 18:03:16 +1100

    created simple linear regression prediction model

--- 01_pytorch_workflow.py
+++ 01_pytorch_workflow.py
@@ -3,26 +3,119 @@ from torch import nn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import random
+# import random
 
-random.seed(42)
+# random.seed(42)
 torch.manual_seed(42)
 torch.cuda.manual_seed_all(42)
 np.random.seed(42)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Using linear regression to create a straight line to have known parameters
-# y = mx + c
-m = 0.7
-c = 0.3
+# torch.no_grad() defines boundaries for code where gradients are not created
+with torch.no_grad():
+    # Using linear regression to create a straight line to have known parameters
+    m = 0.7
+    c = 0.3
 
-start = 0
-end = 1
-step = 0.02
-X = torch.arange(start, end, step).unsqueeze(dim = 1) # makes it a (50, 1), dim = 2
-y = m * X + c
+    start, end, step = 0, 1, 0.01
+    X = torch.arange(start, end, step).unsqueeze(dim = 1) # makes it a (50, 1), dim = 2
+    y = m * X + c # y inherits the shape of X, because * is short notation of element wise multiplication
 
-print(f"{X[:10]}, {y[:10]}")
-print(f"{len(X)}, {len(y)}")
+    X, y = X.to(device), y.to(device) # sending to cuda
 
+    # Shuffling the data
+    indices = torch.randperm(X.size(0)) # random permutation
+    X_shuffled = X[indices].clone() # X[indices] is a view, .clone makes a copy
+    y_shuffled = y[indices].clone()
+    del X, y # deleting X and y to free up memory
+
+    # print(f"{X_shuffled[:10]}, {y_shuffled[:10]}")
+    # print(f"{len(X)}, {len(y)}")
+
+    # a validation set is sometimes useful for checking accuracy during training
+    train_split = int(0.7 * len(X_shuffled))
+    validation_split = int(0.85 * len(X_shuffled))
+
+    train_x, train_y = X_shuffled[:train_split], y_shuffled[:train_split]
+    validation_x, validation_y = X_shuffled[train_split:validation_split], y_shuffled[train_split:validation_split]
+    test_x, test_y = X_shuffled[validation_split:], y_shuffled[validation_split:]
+
+    torch.cuda.empty_cache() # free up memory
+
+
+def plot_predictions(train_data: torch.Tensor = train_x.cpu(), 
+                     train_labels: torch.Tensor = train_y.cpu(), 
+                     validation_data: torch.Tensor = validation_x.cpu(), 
+                     validation_labels: torch.Tensor = validation_y.cpu(), 
+                     test_data: torch.Tensor = test_x.cpu(), 
+                     test_labels: torch.Tensor = test_y.cpu(), 
+                     predictions: torch.Tensor = None):
+    # matplotlib cannot work with gpu tensors, must use .cpu() to make copy
+    
+    plt.figure(figsize = (10, 7))
+    plt.scatter(train_data, train_labels, c = "r", s = 4, label = "Training data")
+    plt.scatter(validation_data, validation_labels, c = "g", s = 4, label = "Validation data")
+    plt.scatter(test_data, test_labels, c = "b", s = 4, label = "Test data")
+
+    if predictions is not None:
+        predictions = predictions.to('cpu', non_blocking=True)
+        plt.scatter(test_data, predictions, c = "y", s = 4, label = "Predictions")
+    
+    plt.legend(prop = {"size": 14})
+    plt.show()
+
+
+# plot_predictions()
+
+# Building a 1 neuron linear regression model
+
+class LinearRegressionModel(nn.Module): # subclass is nn.module
+    def __init__(self):
+        super().__init__() # calls init from parent class
+        self.weight = nn.Parameter(torch.randn(1, 
+                                                requires_grad = True,
+                                                dtype = torch.float32,
+                                                device = device))
+        self.bias = nn.Parameter(torch.randn(1,
+                                             requires_grad = True,
+                                             dtype = torch.float32,
+                                             device = device))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # -> means that it returns a torch tensor
+        return self.weight * x + self.bias
+
+model_0 = LinearRegressionModel().to(device)
+print(model_0.state_dict())
+
+# making predictions with torch.inference_mode()
+
+
+# picking a loss function and optimizer (MAE and stochastic gradient descent)
+loss_fn = nn.L1Loss()
+optimizer = torch.optim.SGD(model_0.parameters(), lr = 0.01)
+
+# training loop and testing loop
+epochs = 187
+
+for epoch in range(epochs):
+    model_0.train() # turns on the training mode for gradients, 
+    
+    pred_y = model_0(train_x) # sets the predictions
+    loss = loss_fn(pred_y, train_y) # first input then target
+    # print(f"Loss: {loss}")
+    optimizer.zero_grad() # clears the gradients so that old grads dont affect current
+    loss.backward() # does the backprop
+    optimizer.step() # makes the optimizer step
+    
+    model_0.eval() # turns off trainig mode for gradients
+    with torch.inference_mode():
+        validation_predictions = model_0(validation_x)
+        validation_loss = loss_fn(validation_predictions, validation_y)
+    print(f"E-{epoch+1} | Loss: {loss:.4f} | Validation loss: {validation_loss:.4f} | Difference: {loss - validation_loss:.4f}")
+
+
+with torch.inference_mode(): # inference mode turns off gradient tracking
+    test_predictions = model_0(test_x)
+plot_predictions(predictions = test_predictions)
